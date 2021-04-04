808cd4c77fc3ccad2c00b0bdb6c778de23064e25
jhunxein <jhunsensamonte@gmail.com>
2021-04-03 20:57:00 -0800

    Open for revision

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/app.css';
 
 import illustrationMobile from './assets/images/illustration-woman-online-mobile-final.svg';
-import bgPatterMobile from './assets/images/bg-pattern-mobile.svg';
+import bgPatternMobile from './assets/images/bg-pattern-mobile.svg';
+
+import illustrationDesktop from './assets/images/illustration-woman-online-desktop.svg';
+import bgPatternDesktop from './assets/images/bg-pattern-desktop.svg';
+import bgBoxDesktop from './assets/images/illustration-box-desktop.svg';
 
 import Faqs from './components/Faqs';
 
 function App() {
+	const [windowSize, setWindowSize] = useState(0);
+
+	const handleWindowResize = () => {
+		const width = window.innerWidth;
+		setWindowSize(width);
+	};
+
+	useEffect(() => {
+		handleWindowResize();
+		window.addEventListener('resize', handleWindowResize);
+		return () => {
+			window.removeEventListener('resize', handleWindowResize);
+		};
+	}, []);
+
 	return (
 		<main className="container">
 			<article className="faq">
-				<div className="ills--wrapper">
-					<img src={illustrationMobile} alt="" className="ills--wrapper__img" />
-					<img src={bgPatterMobile} alt="" className="ills--wrapper__imgBg" />
+				{windowSize >= 980 && (
+					<div className="container--box--wrapper">
+						<img src={bgBoxDesktop} alt="..." className="container--box__img" />
+					</div>
+				)}
+				<div className="faq--info--wrapper">
+					{/* SHOW ILLUSTRATION ON BG */}
+					{windowSize < 980 ? (
+						<div className="faq--ills--wrapper">
+							<img src={illustrationMobile} alt="..." className="faq--ills__img" />
+							<img src={bgPatternMobile} alt="..." className="faq--ills__pattern" />
+						</div>
+					) : (
+						<div className="faq--ills--wrapper">
+							<img src={illustrationDesktop} alt="..." className="faq--ills__img" />
+
+							{/* <img src={bgPatternDesktop} alt="..." className="faq--ills__pattern" /> */}
+						</div>
+					)}
+					<div className="faq--context--wrapper">
+						<h1 className="faq__title">faq</h1>
+						<Faqs />
+					</div>
 				</div>
-				<h1 className="faq__title">faq</h1>
-				<Faqs />
 			</article>
 		</main>
 	);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 
 import 'normalize.css';
 import './css/index.css';
-import './css/buttons.css';
 
 ReactDOM.render(
 	<React.StrictMode>
